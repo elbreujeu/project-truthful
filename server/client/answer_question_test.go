f41941d31380538a6a24cf55bff8cb56e983af62
Julien <julien.barrere@epitech.eu>
2022-10-31 20:21:20 +0100

    feat: Add tests for answer_question

--- server/client/answer_question_test.go
+++ server/client/answer_question_test.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"database/sql"
 	"errors"
 	"project_truthful/client/database"
 	"strings"
@@ -27,17 +28,14 @@ func TestCheckAnswerInfos(t *testing.T) {
 	}
 }
 
-func TestAnswerQuestionEmptyText(t *testing.T) {
+func TestAnswerQuestionError(t *testing.T) {
 	_, _, err := AnswerQuestion(0, 0, "", "ip_address")
 
 	if err == nil {
 		t.Error("Empty text for answer: expected error, got nil")
 	}
-}
 
-func TestAnswerQuestionUserDoesNotExists(t *testing.T) {
 	var mock sqlmock.Sqlmock
-	var err error
 	database.DB, mock, err = sqlmock.New()
 	if err != nil {
 		t.Errorf("Error while creating sqlmock: %s", err.Error())
@@ -54,7 +52,7 @@ func TestAnswerQuestionUserDoesNotExists(t *testing.T) {
 		t.Errorf("User does not exist: expected error, got nil")
 	}
 
-	// database error
+	// check user database error
 	mock.ExpectQuery("SELECT COUNT").WithArgs(2).WillReturnError(errors.New("test error"))
 	_, _, err = AnswerQuestion(2, 0, "toto", "ip_address")
 	if mock.ExpectationsWereMet() != nil {
@@ -62,5 +60,101 @@ func TestAnswerQuestionUserDoesNotExists(t *testing.T) {
 	}
 	if err == nil {
 		t.Errorf("Database error: expected error, got nil")
+	}
+
+	// question does not exists
+	mock.ExpectQuery("SELECT COUNT").WithArgs(3).WillReturnRows(sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(1))
+	mock.ExpectQuery("SELECT receiver_id").WithArgs(1).WillReturnError(sql.ErrNoRows)
+	_, _, err = AnswerQuestion(3, 1, "toto", "ip_address")
+	if mock.ExpectationsWereMet() != nil {
+		t.Errorf("Error while checking expectations: %s", err.Error())
+	}
+	if err == nil {
+		t.Errorf("Question does not exist: expected error, got nil")
+	}
+
+	// check question database error
+	mock.ExpectQuery("SELECT COUNT").WithArgs(4).WillReturnRows(sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(1))
+	mock.ExpectQuery("SELECT receiver_id").WithArgs(2).WillReturnError(errors.New("test error"))
+	_, _, err = AnswerQuestion(4, 2, "toto", "ip_address")
+	if mock.ExpectationsWereMet() != nil {
+		t.Errorf("Error while checking expectations: %s", err.Error())
+	}
+	if err == nil {
+		t.Errorf("Database error: expected error, got nil")
+	}
+
+	// question id and receiver id do not match
+	mock.ExpectQuery("SELECT COUNT").WithArgs(5).WillReturnRows(sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(1))
+	mock.ExpectQuery("SELECT receiver_id").WithArgs(3).WillReturnRows(sqlmock.NewRows([]string{"receiver_id"}).AddRow(2))
+	_, _, err = AnswerQuestion(5, 3, "toto", "ip_address")
+	if mock.ExpectationsWereMet() != nil {
+		t.Errorf("Error while checking expectations: %s", err.Error())
+	}
+	if err == nil {
+		t.Errorf("Question id and receiver id do not match: expected error, got nil")
+	}
+
+	// user already answered the question
+	mock.ExpectQuery("SELECT COUNT").WithArgs(6).WillReturnRows(sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(1))
+	mock.ExpectQuery("SELECT receiver_id").WithArgs(7).WillReturnRows(sqlmock.NewRows([]string{"receiver_id"}).AddRow(6))
+	mock.ExpectQuery("SELECT COUNT").WithArgs(7).WillReturnRows(sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(1))
+	_, _, err = AnswerQuestion(6, 7, "toto", "ip_address")
+	if mock.ExpectationsWereMet() != nil {
+		t.Errorf("Error while checking expectations: %s", err.Error())
+	}
+	if err == nil {
+		t.Errorf("Question id and receiver id do not match: expected error, got nil")
+	}
+
+	// check if question already answered database error
+	mock.ExpectQuery("SELECT COUNT").WithArgs(8).WillReturnRows(sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(1))
+	mock.ExpectQuery("SELECT receiver_id").WithArgs(9).WillReturnRows(sqlmock.NewRows([]string{"receiver_id"}).AddRow(8))
+	mock.ExpectQuery("SELECT COUNT").WithArgs(9).WillReturnError(errors.New("test error"))
+	_, _, err = AnswerQuestion(8, 9, "toto", "ip_address")
+	if mock.ExpectationsWereMet() != nil {
+		t.Errorf("Error while checking expectations: %s", err.Error())
+	}
+	if err == nil {
+		t.Errorf("Question id and receiver id do not match: expected error, got nil")
+	}
+
+	mock.ExpectQuery("SELECT COUNT").WithArgs(10).WillReturnRows(sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(1))
+	mock.ExpectQuery("SELECT receiver_id").WithArgs(11).WillReturnRows(sqlmock.NewRows([]string{"receiver_id"}).AddRow(10))
+	mock.ExpectQuery("SELECT COUNT").WithArgs(11).WillReturnRows(sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(0))
+	mock.ExpectExec("INSERT INTO answer").WithArgs(10, 11, "toto", "ip_address").WillReturnError(errors.New("test error"))
+	_, _, err = AnswerQuestion(10, 11, "toto", "ip_address")
+	if mock.ExpectationsWereMet() != nil {
+		t.Errorf("Error while checking expectations: %s", err.Error())
+	}
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+}
+
+func TestAnswerQuestionSuccess(t *testing.T) {
+	var mock sqlmock.Sqlmock
+	var err error
+	database.DB, mock, err = sqlmock.New()
+	if err != nil {
+		t.Errorf("Error while creating sqlmock: %s", err.Error())
+	}
+
+	mock.ExpectQuery("SELECT COUNT").WithArgs(6).WillReturnRows(sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(1))
+	mock.ExpectQuery("SELECT receiver_id").WithArgs(7).WillReturnRows(sqlmock.NewRows([]string{"receiver_id"}).AddRow(6))
+	mock.ExpectQuery("SELECT COUNT").WithArgs(7).WillReturnRows(sqlmock.NewRows([]string{"COUNT(*)"}).AddRow(0))
+	mock.ExpectExec("INSERT INTO answer").WithArgs(6, 7, "toto", "ip_address").WillReturnResult(sqlmock.NewResult(1, 1))
+	answerId, code, err := AnswerQuestion(6, 7, "toto", "ip_address")
+	if mock.ExpectationsWereMet() != nil {
+		t.Errorf("Error while checking expectations: %s", err.Error())
+	}
+	if err != nil {
+		t.Errorf("Error while answering question: %s", err.Error())
+	}
+	if code != 201 {
+		t.Errorf("Wrong status code: expected 200, got %d", code)
+	}
+	if answerId != 1 {
+		t.Errorf("Wrong answer id: expected 1, got %d", answerId)
 	}
 }
