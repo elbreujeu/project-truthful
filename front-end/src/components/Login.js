d2731bef4f7160bd29b14dc545cf823e862df457
Julien <julien.barrere@epitech.eu>
2023-02-10 00:49:46 +0100

    feat: Awesome design

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { API_URL } from '../Env';
-import '../styles/colors.css';
+import '../styles/style.css';
+import '../styles/Login.css';
 
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -22,7 +23,6 @@ const Login = () => {
         navigate('/feed');
       } else {
         const returnMessage = await response.json();
-        // sets error message as returnMessage.error but capitalizes the first letter
         const errorMessage = returnMessage.error.charAt(0).toUpperCase() + returnMessage.error.slice(1);
         console.error(returnMessage)
         setError(errorMessage);
@@ -36,7 +36,7 @@ const Login = () => {
   return (
     <div className="background-color" style={{display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh'}}>
       <h1 className="text" style={{marginTop: '3rem'}}>Sign in</h1>
-      {error && <div className="error_box">{error}</div>}
+      {error && <div className="error_box" style={{marginBottom: '1rem'}}>{error}</div>}
       <div style={{width: '30%', display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
         <label className='text' style={{alignSelf: 'flex-start', marginBottom: '0.5rem'}}>Username</label>
           <input 
@@ -45,25 +45,23 @@ const Login = () => {
               className='text_box'
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              style={{width: '100%', padding: '0.5rem', marginTop: '0'}} 
           />
-        <label className='text' style={{alignSelf: 'flex-start', marginBottom: '0.5rem'}}>Password</label>
+        <label className='text' style={{alignSelf: 'flex-start', marginTop:'0.5rem', marginBottom: '0.5rem'}}>Password</label>
         <input 
             type="password" 
             id="password" 
             className='text_box'
             value={password} 
             onChange={(e) => setPassword(e.target.value)}
-            style={{width: '100%', padding: '0.5rem', marginTop: '0'}} 
         />
         <button 
             className="button" 
-            style={{width: '100%', padding: '1rem', marginTop: '2rem'}}
+            style={{padding: '1rem', marginTop: '2rem', alignSelf: 'center', fontFamily: 'Fira Code', fontSize: '1rem'}}
             onClick={handleLogin}
         >
             Login
         </button>
-        <p className="alt_text" style={{marginTop: '0rem'}}>Forgot password ?</p>
+        <p className="alt_text" style={{marginTop: '0rem', alignSelf: 'center'}}>Forgot password ?</p>
       </div>
     </div>
   );
