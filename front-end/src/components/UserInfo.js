eac03fc32b25648543e9d095d3a1ff3c82dd6aaf
Julien <julien.barrere@epitech.eu>
2024-05-13 20:16:34 +0200

    feat: Unfollow button
    Temporary workaround to showcase

--- front-end/src/components/UserInfo.js
+++ front-end/src/components/UserInfo.js
@@ -49,6 +49,47 @@ const UserInfo = (userData) => {
                 // Handle error here
             });
     };
+    const unfollowUser = () => {
+        console.log('Unfollow user')
+        // gets the "token" cookie
+        const cookieElement = document.cookie.split('; ').find(row => row.startsWith('token='));
+        const token = cookieElement ? cookieElement.split('=')[1] : null;
+        if (!token) {
+            console.error('No token found');
+            window.location.href = '/login';
+        }
+        // Backend POST request to API_URL/follow_user
+        // if request is unsuccessful, console.error the error and put red textbox above the page
+        fetch(`${API_URL}/follow_user`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({ user_id: userInfo.id, follow: false})
+        })
+            .then(response => {
+                // gets the response json
+                if (!response.ok) {
+                    response.json().then(data => {
+                        console.error(data.error);
+                        // sets the error box to the error message
+                        setErrorBox(data.error);
+                    });
+                } else {
+                    response.json().then(data => {
+                        console.log(data);
+                        // sets the success box to the success message
+                        setSuccessBox(data.message);
+                    });
+                }
+                // Handle successful response here
+            })
+            .catch(error => {
+                console.error(error);
+                // Handle error here
+            });
+    };
     return (
         <div className="user-info">
             {errorBox && <div className="error_box" style={{marginBottom: '1rem'}}>{errorBox}</div>}
@@ -65,7 +106,10 @@ const UserInfo = (userData) => {
                 {userInfo.answer_count} answers {/* Answer count */}
                 <a href={`/profile/${userInfo.username}/following`}>{userInfo.following_count} following</a> {/* Following count */}
             </div>
-            <button className="button" onClick={followUser}>Follow</button> {/* Follow button TODO : add a route in backend to check if the user is following the user. If so, turn the button into an "unfollow button */}
+            {/* Follow button TODO : add a route in backend to check if the user is following the user. If so, turn the button into an "unfollow button */}
+            <button className="button" onClick={followUser}>Follow</button>
+            <p></p>
+            <button className="button" onClick={unfollowUser}>Unfollow</button>
         </div>
     );
 };
