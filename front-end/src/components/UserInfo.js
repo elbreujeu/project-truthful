82f2ccd7058e5de541bcc018d8e84f5d3e586275
Julien <julien.barrere@epitech.eu>
2024-05-13 18:34:38 +0200

    feat: Follow user button

--- front-end/src/components/UserInfo.js
+++ front-end/src/components/UserInfo.js
@@ -1,16 +1,58 @@
 import React from 'react';
 import '../styles/style.css';
 import '../styles/UserInfo.css';
+import { API_URL } from '../Env';
+import { useState } from 'react';
 
 const UserInfo = (userData) => {
     const userInfo = userData.userInfo;
+    const [errorBox, setErrorBox]  = useState('');
+    const [successBox, setSuccessBox]  = useState('');
     const followUser = () => {
         console.log('Follow user')
+        // gets the "token" cookie
+        const cookieElement = document.cookie.split('; ').find(row => row.startsWith('token='));
+        const token = cookieElement ? cookieElement.split('=')[1] : null;
+        if (!token) {
+            console.error('No token found');
+            window.location.href = '/login';
+        }
         // Backend POST request to API_URL/follow_user
-        // TODO : Write backend call here
+        // if request is unsuccessful, console.error the error and put red textbox above the page
+        fetch(`${API_URL}/follow_user`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({ user_id: userInfo.id, follow: true})
+        })
+            .then(response => {
+                // gets the response json
+                if (!response.ok) {
+                    response.json().then(data => {
+                        console.error(data.error);
+                        // sets the error box to the error message
+                        setErrorBox(data.error);
+                    });
+                } else {
+                    response.json().then(data => {
+                        console.log(data);
+                        // sets the success box to the success message
+                        setSuccessBox(data.message);
+                    });
+                }
+                // Handle successful response here
+            })
+            .catch(error => {
+                console.error(error);
+                // Handle error here
+            });
     };
     return (
         <div className="user-info">
+            {errorBox && <div className="error_box" style={{marginBottom: '1rem'}}>{errorBox}</div>}
+            {successBox && <div className="success_box" style={{marginBottom: '1rem'}}>{successBox}</div>}
             <div className="profile-picture"> {/* Wrap the image in a .profile-picture element */}
                 <img src="https://i.imgur.com/Jvh1OQm.jpeg" alt="Profile" />
             </div>
