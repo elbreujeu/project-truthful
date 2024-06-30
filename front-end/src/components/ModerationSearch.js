fe8d6591a2b98b027b99bb9a9bee7c442762b4fe
Julien <julien.barrere@epitech.eu>
2024-06-30 15:49:48 +0200

    feat : display user info

--- front-end/src/components/ModerationSearch.js
+++ front-end/src/components/ModerationSearch.js
@@ -5,6 +5,7 @@ import { API_URL } from '../Env';
 function ModerationSearch() {
     const [inputValue, setInputValue] = useState('');
     const [error, setError] = useState('');
+    const [userInfo, setUserInfo] = useState(null);
 
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
@@ -15,6 +16,7 @@ function ModerationSearch() {
         // Handle the form submission here
         if (inputValue.trim() === '') {
             setError('Please enter a value');
+            setUserInfo(null);
             return false;
         }
         return true;
@@ -27,29 +29,35 @@ function ModerationSearch() {
         fetch(`${API_URL}/get_user_profile/${inputValue}`)
             .then(response => {
                 if (response.ok) {
-                    // handle the ok response
+                    response.json().then(data => {
+                        setUserInfo(data);
+                    });
                 } else if (response.status === 404) {
                     setError('User not found');
+                    setUserInfo(null);
                 } else if (response.headers.get('content-type').includes('application/json')) {
                     response.json().then(data => {
                         setError(data.message);
+                        setUserInfo(null);
                     });
                 } else {
                     setError('An error occurred');
+                    setUserInfo(null);
                 }
             })
             .catch(error => {
                 console.error('Error:', error);
                 setError('Error reaching server');
+                setUserInfo(null);
             });
-    }
+    };
 
     const handleSubmit = (e) => {
         if (!CheckInputEmpty(e)) {
             return;
         }
         setError('');
-        console.log(LookUpUser(e));
+        LookUpUser(e);
     };
 
     return (
@@ -66,6 +74,16 @@ function ModerationSearch() {
                 />
                 <button type="submit">Submit</button>
             </form>
+            {userInfo && (
+                <div>
+                    <h2>User Information</h2>
+                    <p>Username: {userInfo.username}</p>
+                    <p>Display name: {userInfo.display_name}</p>
+                    <p>Number of answered questions: {userInfo.answer_count}</p>
+                    <p>Number of followers: {userInfo.follower_count}</p>
+                    <p>Number of followings: {userInfo.following_count}</p>
+                </div>
+            )}
         </div>
     );
 }
